411d7a934c5ec12292e029c15fc4aab8051a52ae
jjcarrcannings <jjcarrcannings@gmail.com>
2023-06-12 23:32:37 -0700

    added login page test

--- test/locators.py
+++ test/locators.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class LoginPageLocators(object):
-    
+    NETLINK_INPUT = (By.XPATH, '//input[1]')
+    PASSWORD_INPUT = (By.XPATH, '//input[2]')
+    SIGNIN_BUTTON = (By.XPATH, "//div[text()='SIGN IN']/parent::button")
+    FORGOT_PASSWORD_LINK = (By.XPATH, "//div[contains(text(),'Forgot your')]")
--- test/test_cases/ui_integration.py
+++ test/test_cases/ui_integration.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import page
 
 import unittest
 
@@ -8,16 +9,17 @@ class ChromeTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome('./webdrivers/chromedriver.exe')
+        cls.driver = webdriver.Chrome('../webdrivers/chromedriver.exe')
         return super().setUpClass()
     
     def setUp(self):
-        self.driver.get('https://google.ca')
+        self.login_page = page.LoginPage(self.driver)
+        self.driver.get('http://localhost:3000')
         return super().setUp()
     
-    def test_landing_page(self):
-        self.driver.find_element(By.XPATH, "//input[@value='Google Search']")
-        print('Found element.')
+    def test_login_page(self):
+        self.login_page.login('test123','test123')
+        print('Login complete.')
 
 
     @classmethod
