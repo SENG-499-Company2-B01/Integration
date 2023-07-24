1b1b346a36169aafa95810e83b5eaa6c9dcbf506
mattmc-00 <105465876+mattmc-00@users.noreply.github.com>
2023-07-24 16:57:34 -0700

    Added logic for testall cli function (#16)
    * Added logic for testall cli function
    
    * Added env variable logic for swapping services
    
    * Missed django env variables for algs2
    
    * Updated docker container names, still need name of company1 algs2 container
    
    * Added all combinations to testall function
    
    * Got name for algs2 container, but still getting errors working with company 1 containers
    
    * Dynamically writing .env files to update env variables

--- integration.py
+++ integration.py
@@ -2,9 +2,7 @@ import json
 import subprocess
 import os
 import logging
-import select
 import subprocess
-import sys
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -25,6 +23,89 @@ services = {
     'algs1': 0,
     'algs2': 0
 }
+
+company1_backend_algs1 = {
+    1 : "http://localhost:5001,http://localhost:5001",
+    2 : "http://localhost:8017,http://localhost:8017"
+}
+
+company1_backend_algs2 = {
+    1 : "http://localhost:8080,http://localhost:8080",
+    2 : "http://localhost:8001,http://localhost:8001"
+}
+
+company2_backend_algs1 = {
+    1 : "http://localhost:5001/generate",
+    2 : "http://localhost:8017/generate"
+}
+
+company2_backend_algs2 = {
+    1 : "http://localhost:8080/predict",
+    2 : "http://localhost:8001/predict"
+}
+
+def write_env_file(env_file_path, env_variables):
+    with open(env_file_path, "w") as f:
+        for key in env_variables:
+            f.write(f'"{key}"="{env_variables[key]}"\n')
+
+def set_frontend_env_variables():
+    env_variables = {
+        "REACT_APP_BACKEND_URL" : "http://localhost:8000"
+    }
+    write_env_file("company2/frontend/.env", env_variables)
+
+def set_company1_env_variables(algs1_link, algs2_link):
+    env_variables = {
+        "DATABASE_URI" : "postgresql+psycopg2://admin:password@db:5432/uvic",
+        "APP_SETTINGS" : "ProductionConfig",
+        "FLASK_RUN_HOST" : "0.0.0.0",
+        "FLASK_APP" : "app.py",
+        "FLASK_DEBUG" : "1",
+        "APP_VERSION" : "$APP_VERSION",
+        "ALG1_URLs" : algs1_link,
+        "ALG2_URLs" : algs2_link
+    }
+    write_env_file("company1/backend/.env", env_variables)
+
+def set_company2_env_variables(algs1_link, algs2_link):
+    env_variables = {
+        "ENVIRONMENT" : "development",
+        "MONGO_LOCAL_HOST" : "10.9.0.3:27017",
+        "MONGO_LOCAL_USERNAME" : "admin",
+        "MONGO_LOCAL_PASSWORD" : "admin",
+        "ADMIN_1" : "rich.little ",
+        "ADMIN_2" : "dan.mai",
+        "JWT_SECRET" : "secret",
+        "API_HASH" : "fe80decbd03b2933f3d7eba3079e6b3e7c1bb2e3613f3671388c969fd6cd5aca",
+        "HOST_NAME" : "http://localhost",
+        "DJANGO_MODE" : "dev",
+        "DJANGO_KEY" : "4104c7d331cb642a222340cd5324b4f2",
+        "ALG1_URLs" : algs1_link,
+        "ALG2_URLs" : algs2_link
+    }
+    write_env_file("company2/backend/.env", env_variables)
+
+def set_company1_algs2_env_variables():
+    env_variables = {
+    }
+    write_env_file("company1/algs2/.env", env_variables)
+
+def set_company2_algs2_env_variables():
+    env_variables = {
+        "DJANGO_MODE" : "dev",
+        "SECRET_KEY" : "4104c7d331cb642a222340cd5324b4f2",
+        "HOST_IP" : '127.0.0.1',
+        "HOST_NAME" : 'www.example.com',
+        "BACKEND_URL" : 'http://localhost:8000'
+    }
+    write_env_file("company2/algs2/.env", env_variables)
+
+def update_backend_env_variables(backend_company, algs1_company, algs2_company):
+    if backend_company == 1:
+        set_company1_env_variables(company1_backend_algs1[algs1_company], company1_backend_algs2[algs2_company])
+    elif backend_company == 2:
+        set_company2_env_variables(company2_backend_algs1[algs1_company], company2_backend_algs2[algs2_company])
 
 def load_config():
     try:
@@ -212,6 +293,9 @@ def swap_service(service_name):
     # The company that should run the service next
     next_company = (current_company % num_companies) + 1
 
+    # Change backend env variables for swapped service
+    update_backend_env_variables(service_name, next_company)
+
     # Stop the service for the current company
     if not kill_service(service_name):
         logger.warning(f"Failed to kill {service_name} for company{current_company}.")
@@ -267,7 +351,24 @@ def test():
 
 
 def autotest_all():
-    logger.info("This has not been implemented yet")
+    logger.info("Killing all currently running services")
+    kill_all_services()
+    logger.info(f"Running default Company 2 configuration")
+    run_company(2)
+    logger.info(f"Testing default Company 2 configuration")
+    test()
+    for backend in [1, 2]:
+        if services['backend'] != backend:
+            swap_service("backend")
+        for algs1 in [1, 2]:
+            if services['algs1'] != algs1:
+                swap_service("backend")
+            for algs2 in [1, 2]:
+                if services['algs2'] != algs2:
+                    swap_service("algs2")
+                test()
+
+    logger.info("Finished all combinations of Company 1 and 2 tests")
 
 
 def handle_run(args):
@@ -365,6 +466,11 @@ def main():
     # Clone and build all services, if necessary
     clone_all_services()
     #build_all()
+
+    # Init frontend env variable
+    set_frontend_env_variables()
+    set_company1_algs2_env_variables()
+    set_company2_algs2_env_variables()
 
     # Run company 2 containers by default
     # run_company(COMPANIES['company2'])
